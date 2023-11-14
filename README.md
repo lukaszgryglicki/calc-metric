0fbc6a7a8fc7838c7acdf810d9246f13dc931725
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-11-14 06:56:06 +0000

    WIP 2
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 This program is used to calculate a metrc and save its values to a dedicated table.
 
+You specify environment variables starting with `V3_` prefix to specify which metric shoudl be calculated.
+
+Those are mandatory parameters that must be specified:
+
+- `V3_PROJECT_SLUG` - specifies project slug to calculate, example: `korg`.
--- cmd/calcmetric/calcmetric.go
+++ cmd/calcmetric/calcmetric.go
@@ -1,13 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	lib "github.com/lukaszgryglicki/calcmetric"
 )
 
+const (
+	gPrefix = "V3_"
+)
+
+var (
+	gRequired = []string{"PROJECT_SLUG"}
+)
+
+func calcMetric() error {
+	env := make(map[string]string)
+	prefixLen := len(gPrefix)
+	for _, pair := range os.Environ() {
+		if strings.HasPrefix(pair, gPrefix) {
+			ary := strings.Split(pair, "=")
+			if len(ary) != 2 {
+				continue
+			}
+			env[ary[0][prefixLen:]] = ary[1]
+		}
+	}
+	_, debug := env["DEBUG"]
+	if debug {
+		lib.Logf("map: %+v\n", env)
+	}
+	for _, key := range gRequired {
+		_, ok := env[key]
+		if !ok {
+			msg := fmt.Sprintf("you must define %s%s environment variable to run this", gPrefix, key)
+			lib.Logf("%s\n", msg)
+			err := fmt.Errorf("%s", msg)
+			return err
+		}
+	}
+	return nil
+}
+
 func main() {
 	dtStart := time.Now()
+	err := calcMetric()
+	if err != nil {
+		lib.Logf("calcMetric error: %+v\n", err)
+	}
 	dtEnd := time.Now()
 	lib.Logf("Time: %v\n", dtEnd.Sub(dtStart))
 }
