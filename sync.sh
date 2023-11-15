b835e978f23ac54c49744c11e9e0d874bcf89239
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-11-15 18:40:08 +0000

    WIP 52
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- sync.sh
+++ sync.sh
@@ -10,6 +10,7 @@ fi
 # export V3_THREADS=1
 export V3_THREADS=3
 ./sync
+echo "Sync done, exit status: $?"
 # clear && V3_CONN="`cat ./REPLICA.secret`" ./sync.sh 1>> sync.log 2>> sync.err &
 # clear && tail -f sync.???
 # grep -E 'sync|calcmetric'
