1e40edca2a69ab5c0e7fe62f30304fc7b0ecd1f8
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-11-15 12:06:24 +0000

    WIP 47
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- cmd/sync/sync.go
+++ cmd/sync/sync.go
@@ -356,12 +356,12 @@ func processTask(ch chan error, idx int, debug bool, binCmd string, tasks []map[
 		err = fmt.Errorf("%s", msg)
 	} else {
 		if !debug {
-			lib.Logf("task #%d finished in %v (skipped: %v), details:\n", idx, took, skipped)
+			lib.Logf("task #%d finished in %v (skipped or no data: %v), details:\n", idx, took, skipped)
 			prettyPrintTask(task)
 		}
 	}
 	if debug {
-		lib.Logf("task #%d (%+v) executed (skipped: %v), took: %v\n", idx, task, skipped, dtEnd.Sub(dtStart))
+		lib.Logf("task #%d (%+v) executed (skipped or no data: %v), took: %v\n", idx, task, skipped, dtEnd.Sub(dtStart))
 	}
 	if ch != nil {
 		ch <- err
