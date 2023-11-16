9c4ad318660063c261953e42b3e05b3dbf50ec59
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-11-16 12:42:36 +0000

    WIP 61
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- examples/sql/progress.sql
+++ examples/sql/progress.sql
@@ -1,1 +1,24 @@
-select project_slug, time_range, min(date_from) as from, max(date_to) as to, max(row_number) as max_row, count(*) as records, max(last_calculated_at) as calculated_at, max(all_contributions) as all_contributions, max(all_contributors) as all_contributors, avg(percent_total) as avg_percent_total, avg(change_from_previous) as avg_change_from_previous from metric_contr_lead_acts_nbot group by project_slug, time_range order by calculated_at desc limit 50
+select
+  project_slug,
+  time_range,
+  metric,
+  min(date_from) as date_from,
+  max(date_to) as date_to,
+  max(row_number) as max_row,
+  count(*) as records,
+  max(last_calculated_at) as calculated_at,
+  max(all_contributions) as all_contributions,
+  max(all_contributors) as all_contributors,
+  avg(percent_total) as avg_percent_total,
+  avg(change_from_previous) as avg_change_from_previous
+from
+  metric_contr_lead_nbot
+group by
+  project_slug,
+  time_range,
+  metric
+order by
+  calculated_at desc
+limit
+  60
+;
